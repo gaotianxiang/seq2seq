daf3eacd3f4838467bf1c702f43a95ce88dc0096
Tianxiang Gao <gtx9726@gmail.com>
2019-03-27 14:33:57 -0400

    add model/net.py copy EncoderRNN and DecoderRNN into it and add the device argument to the init_hidden method

---
+++ model/net.py
@@ -0,0 +1,40 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class EncoderRNN(nn.Module):
+    def __init__(self, input_vocabulary_size, hidden_size):
+        super().__init__()
+        self.hidden_size = hidden_size
+        self.embedding = nn.Embedding(input_vocabulary_size, hidden_size)
+        self.gru = nn.GRU(hidden_size, hidden_size)
+
+    def forward(self, input, hidden):
+        embeded = self.embedding(input).view(1, 1, -1)
+        output = embeded
+        output, hidden = self.gru(output, hidden)
+        return output, hidden
+
+    def init_hidden(self, device):
+        return torch.zeros(1, 1, self.hidden_size, device=device)
+
+
+class DecoderRNN(nn.Module):
+    def __init__(self, hidden_size, output_vocabulary_size):
+        super().__init__()
+        self.hidden_size = hidden_size
+        self.embedding = nn.Embedding(output_vocabulary_size, hidden_size)
+        self.gru = nn.GRU(hidden_size, hidden_size)
+        self.out = nn.Linear(hidden_size, output_vocabulary_size)
+        self.softmax = nn.LogSoftmax(dim=1)
+
+    def forward(self, input, hidden):
+        output = self.embedding(input).view(1, 1, -1)
+        output = F.relu(output)
+        output, hidden = self.gru(output, hidden)
+        output = self.softmax(self.out(output[0]))
+        return output, hidden
+
+    def init_hidden(self, device):
+        return torch.zeros(1, 1, self.hidden_size, device=device)
