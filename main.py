5692a4c45c5a7ee87f306bcbba6bea28823e63bb
Tianxiang Gao <gtx9726@gmail.com>
2019-03-27 15:06:17 -0400

    copy evaluate part to the evaluate.py

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
-import unicodedata
 import os
-import re
 import random
 import argparse
 
 import torch
 import torch.nn as nn
 from torch import optim
-import torch.nn.functional as F
 from tqdm import tqdm
 from utils import RunningAverage
 from model.net import EncoderRNN, DecoderRNN
@@ -137,53 +134,4 @@ encoder1 = EncoderRNN(input_lang.n_words, hidden_size).to(device)
 decoder1 = DecoderRNN(hidden_size, output_lang.n_words).to(device)
 
 
-# train_iters(encoder1, decoder1, 75000, print_every=100)
-
-
-def evaluate(encoder: EncoderRNN, decoder: DecoderRNN, sentence, max_length=MAX_LENGTH):
-    with torch.no_grad():
-        input_tensor = tensor_from_sentence(input_lang, sentence)
-        input_length = input_tensor.size()[0]
-        encoder_hidden = encoder.init_hidden(device)
-
-        encoder_outputs = torch.zeros(max_length, encoder.hidden_size, device=device)
-
-        for ei in range(input_length):
-            encoder_output, encoder_hidden = encoder(input_tensor[ei], encoder_hidden)
-
-        decoder_input = torch.tensor([[SOS_token]], device=device)
-
-        decoder_hidden = encoder_hidden
-
-        decoded_words = []
-
-        for di in range(max_length):
-            decoder_output, decoder_hidden = decoder(decoder_input, decoder_hidden)
-            topv, topi = decoder_output.topk(1)
-            if topi.item() == EOS_token:
-                decoded_words.append('<EOS>')
-                break
-            else:
-                decoded_words.append(output_lang.index2word[topi.item()])
-            decoder_input = topi.squeeze().detach()
-
-        return decoded_words
-
-
-def evaluate_randomly(encoder: EncoderRNN, deocder: DecoderRNN, n=10):
-    for i in range(n):
-        pair = random.choice(pairs)
-        print('>', pair[0])
-        print('=', pair[1])
-        output_words = evaluate(encoder, deocder, pair[0])
-        output_sentence = ' '.join(output_words)
-        print('<', output_sentence)
-        print('')
-
-
-print('loading model...')
-state = torch.load('./ckpts/best.pth.tar', map_location='cpu')
-encoder1.load_state_dict(state['encoder'])
-decoder1.load_state_dict(state['decoder'])
-print('model loaded, start translating...')
-evaluate_randomly(encoder1, decoder1)
+train_iters(encoder1, decoder1, 75000, print_every=100)
