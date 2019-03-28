3fd699dcf31bf0466e97d6a7626369398d017652
Tianxiang Gao <gtx9726@gmail.com>
2019-03-28 09:44:00 -0400

    num_worker = 1 can run

--- train.py
+++ train.py
@@ -5,7 +5,7 @@ import argparse
 import torch
 import torch.nn as nn
 from torch import optim
-from tqdm import tqdm
+from tqdm import tqdm, trange
 from utils import RunningAverage
 from model.net import EncoderRNN, DecoderRNN
 from build_dataset import prepare_data, EOS_token, SOS_token, MAX_LENGTH
@@ -22,12 +22,31 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 input_lang, output_lang, pairs = prepare_data('eng', 'fra', reverse=True)
 print(random.choice(pairs))
 
+from torch.utils import data as data
+
+
+class Fra2Eng(data.Dataset):
+    def __init__(self, pairs):
+        super().__init__()
+        self.fra2eng_pairs = pairs
+
+    def __len__(self):
+        return len(self.fra2eng_pairs)
+
+    def __getitem__(self, idx):
+        return self.fra2eng_pairs[idx]
+
 
 def tensor_from_pair(pair):
     input_tensor = tensor_from_sentence(input_lang, pair[0], device=device)
     output_tensor = tensor_from_sentence(output_lang, pair[1], device=device)
     return input_tensor, output_tensor
 
+
+pairs = [tensor_from_pair(pair) for pair in pairs]
+pairs = Fra2Eng(pairs)
+
+pairs = data.DataLoader(pairs, batch_size=1, shuffle=True, num_workers=8)
 
 teacher_forcing_ratio = 0.5
 
@@ -39,7 +58,6 @@ def train(input_tensor,
           encoder_optimizer: optim.Optimizer,
           decoder_optimizer: optim.Optimizer,
           criterion, max_length=MAX_LENGTH):
-
     encoder_hidden = encoder.init_hidden(device)
     encoder_optimizer.zero_grad()
     decoder_optimizer.zero_grad()
@@ -82,45 +100,52 @@ def train(input_tensor,
 
 def train_iters(encoder: EncoderRNN,
                 decoder: DecoderRNN,
-                n_iters,
+                epochs,
+                pairs,
                 print_every=1000,
                 learning_rate=0.01):
-
     loss_avg = RunningAverage()
     current_best_loss = 1e3
 
     encoder_optimizer = optim.SGD(encoder.parameters(), lr=learning_rate)
     decoder_optimizer = optim.SGD(decoder.parameters(), lr=learning_rate)
 
-    training_pairs = [tensor_from_pair(random.choice(pairs)) for _ in range(n_iters)]
+    # training_pairs = [tensor_from_pair(random.choice(pairs)) for _ in range(n_iters)]
+    training_pairs = pairs
 
     criterion = nn.NLLLoss()
 
-    with tqdm(total=n_iters) as progress_bar:
-        for iter in range(1, n_iters + 1):
-            training_pair = training_pairs[iter - 1]
-            input_tensor = training_pair[0]
-            target_tensor = training_pair[1]
+    for epoch in trange(epochs, desc='epochs'):
+        i = 0
+        with tqdm(total=len(training_pairs)) as progress_bar:
+            # for iter in range(1, len(training_pairs) + 1):
+            #     training_pair = training_pairs[iter - 1]
+            #     input_tensor = training_pair[0]
+            #     target_tensor = training_pair[1]
 
-            loss = train(input_tensor, target_tensor, encoder, decoder, encoder_optimizer, decoder_optimizer, criterion)
-            loss_avg.update(loss)
+            for input_tensor, target_tensor in training_pairs:
+                input_tensor, target_tensor = input_tensor.to(device), target_tensor.to(device)
+                loss = train(input_tensor[0], target_tensor[0], encoder, decoder, encoder_optimizer, decoder_optimizer,
+                             criterion)
+                loss_avg.update(loss)
+                i += 1
+                if i % print_every == 0:
+                    tqdm.write('# of iterations: {}, loss average: {:.3f}'.format(i, loss_avg.avg))
+                    if loss_avg.avg < current_best_loss:
+                        tqdm.write('new best loss average found, saving model...')
+                        current_best_loss = loss_avg.avg
+                        state = {
+                            'encoder': encoder.state_dict(),
+                            'decoder': decoder.state_dict(),
+                            'iters': i,
+                            'epoch': epoch
+                        }
+                        os.makedirs('./ckpts', exist_ok=True)
+                        torch.save(state, './ckpts/best.pth.tar')
+                    loss_avg.reset()
 
-            if iter % print_every == 0:
-                tqdm.write('# of iterations: {}, loss average: {:.3f}'.format(iter, loss_avg.avg))
-                if loss_avg.avg < current_best_loss:
-                    tqdm.write('new best loss average found, saving model...')
-                    current_best_loss = loss_avg.avg
-                    state = {
-                        'encoder': encoder.state_dict(),
-                        'decoder': decoder.state_dict(),
-                        'iters': iter
-                    }
-                    os.makedirs('./ckpts', exist_ok=True)
-                    torch.save(state, './ckpts/best.pth.tar')
-                loss_avg.reset()
-
-            progress_bar.set_postfix(loss_avg=loss_avg.avg)
-            progress_bar.update()
+                progress_bar.set_postfix(loss_avg=loss_avg.avg)
+                progress_bar.update()
 
 
 if __name__ == '__main__':
@@ -128,4 +153,4 @@ if __name__ == '__main__':
     encoder1 = EncoderRNN(input_lang.n_words, hidden_size).to(device)
     decoder1 = DecoderRNN(hidden_size, output_lang.n_words).to(device)
 
-    train_iters(encoder1, decoder1, 75000, print_every=100)
+    train_iters(encoder1, decoder1, 100, pairs, print_every=100)
